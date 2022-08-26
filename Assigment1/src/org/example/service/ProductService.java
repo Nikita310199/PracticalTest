1bb40763e89c12c173f554af0305bbad20cf763e
Nikita <Nikita@DESKTOP-A12TC54>
2022-08-26 22:26:02 +0530

    Assigment1

---
+++ Assigment1/src/org/example/service/ProductService.java
@@ -0,0 +1,17 @@
+
+package org.example.service;
+import org.example.model.Product;
+public class ProductService {
+	public void increasePrice(Product p)   
+ 
+  {
+     double newprice;
+	 System.out.println(p.getPrice());  //print the price in Product class
+	 newprice=p.getPrice()+100; //increase the price in 100 to the original price
+   
+ 
+     System.out.println("increase Price is"+newprice); //print the increase price
+ 
+	
+  }
+}
